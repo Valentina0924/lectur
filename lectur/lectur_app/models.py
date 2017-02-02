d0c150bd59f994886ced9d7cc30bfd0949b635e6
Valentina0924 <tina-h0924@hotmail.com>
2017-02-02 11:41:33 -0500

    Add files via upload

--- lectur/lectur_app/models.py
+++ lectur/lectur_app/models.py
@@ -1,3 +1,126 @@
-from django.db import models
+#encoding:utf-8
+from __future__ import unicode_literals
 
-# Create your models here.,x
+import string
+import random
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Genero(models.Model):
+    titulo = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=100, unique=True)
+    def __unicode__(self):
+            return self.title
+
+
+class Lector(models.Model):
+    user = models.OneToOneField(User);
+    codigo= models.CharField(max_length=128); #17010001 año(2 últimos dígitos) mes(2 dígitos) cifra(4 dígitos)
+    facebook = models.URLField(blank=True);
+    twitter = models.URLField(blank=True);
+    descripcion = models.TextField(max_length=400);
+    fecha_ingreso = models.DateField(auto_now_add=True);
+    imagen = models.ImageField(upload_to='profile_images/');
+    def __unicode__(self):
+        return unicode(self.codigo+" "+self.user.username);
+
+class Valoracion(models.Model):
+    """docstring for Valoracion."""
+    lector = models.ForeignKey(Lector);
+    opinion = models.TextField(max_length=400, blank=True);
+    calificacion = models.DecimalField(max_digits=3, decimal_places=1,default=0.0)
+    def __unicode__(self):
+        return unicode(self.codigo+" "+self.user.username);
+
+
+class Taller(models.Model):
+    organizador = models.ForeignKey(Lector);
+    fecha = models.DateTimeField();
+    descripcion = models.TextField(max_length=400);
+    imagen = models.ImageField(upload_to='talleres_images/');
+    nombre = models.CharField(max_length=100); # Nombre del taller
+    slug = models.SlugField(max_length=100, unique=True) # Nombre del taller sin carácteres espeiales
+    cupos = models.IntegerField(default=0)
+    participantes = models.ManyToManyField(Lector, related_name='participantes_taller');
+    def __unicode__(self):
+        return unicode(self.nombre+" "+self.organizador.user.username);
+
+class Libro(models.Model):
+    """docstring for Libro."""
+    titulo = models.CharField(max_length=128);
+    autor = models.CharField(max_length=128);
+    portada = models.ImageField(upload_to='libros_images/');
+    descripcion = models.TextField(max_length=400);
+    generos = models.ManyToManyField(Genero, related_name='generos_libro');
+    likes = models.ManyToManyField(User, related_name='likes_libro');
+    valoraciones= models.ManyToManyField(Valoracion, related_name='valoraciones_libro');
+    url = models.URLField(blank=True, null=True);
+    def __unicode__(self):
+        return unicode(self.titulo+" "+self.autor);
+
+class Trabajo_Escrito(models.Model):
+    """docstring for Libro."""
+    titulo = models.CharField(max_length=128);
+    autor = models.ForeignKey(Lector);
+    descripcion = models.TextField(max_length=400);
+    texto = models.TextField();
+    generos = models.ManyToManyField(Genero, related_name='generos_escrito');
+    likes = models.ManyToManyField(User, related_name='likes_escrito');
+    valoraciones= models.ManyToManyField(Valoracion, related_name='valoraciones_escrito');
+    isActive = models.BooleanField(default=True); # el texto se ha eliminado
+    isPublic = models.BooleanField(default=True); # el texto es publico
+    def __unicode__(self):
+        return unicode(self.titulo+" "+self.autor.user.username);
+
+class Espacio(models.Model):
+    tipo = models.IntegerField(default = 1); # 0:Biblioteca 1:sala, 2: casa, 3: parque, 4:cafeteria
+    direccion = models.CharField(max_length=40);
+    latitud = models.DecimalField(max_digits=20, decimal_places=10,default=0.0);
+    longitud = models.DecimalField(max_digits=20, decimal_places=10,default=0.0);
+    nombre = models.CharField(max_length=20);
+    descripcion = models.CharField(max_length=300);
+    def __unicode__(self):
+        return unicode(self.tipo+" "+self.nombre +" "+self.direccion);
+
+class Evento(models.Model):
+    dias = models.CharField(max_length=22); # "LU MA MI JU VI SA DO"
+    hora_inicio = models.TimeField();
+    hora_fin = models.TimeField();
+    nombre = models.CharField(max_length=20);
+    descripcion = models.CharField(max_length=200);
+    tipo_repeticion = models.IntegerField(default=1); # 0:diario, 1:semanal, 2:mesual, 3:anual
+    def __unicode__(self):
+        return unicode(self.nombre+" "+self.dias);
+
+class Info_Libro(models.Model):
+    libro = models.ForeignKey(Libro);
+    cantidad_total = models.IntegerField(default = 1);
+    cantidad_disponible = models.IntegerField(default = 1);
+    lectuales = models.ManyToManyField(Lector, related_name = 'lectores_actuales');
+    def __unicode__(self):
+        return unicode(self.libro);
+
+class Biblioteca(models.Model):
+    nombre = models.CharField(max_length=20);
+    ubicacion = models.ForeignKey(Espacio);
+    programacion = models.ManyToManyField(Evento, related_name = 'programacion_biblioteca');
+    administradores = models.ManyToManyField(Lector, related_name = 'administradores_biblioteca');
+    libros = models.ManyToManyField(Info_Libro, related_name = 'libros_biblioteca');
+    espacios = models.ManyToManyField(Espacio, related_name = 'espacios_bibliotecas');
+    def __unicode__(self):
+        return unicode(self.nombre);
+
+class Comunidad(models.Model):
+    administradores = models.ManyToManyField(Lector, related_name = 'administradores_comunidad');
+    participantes = models.ManyToManyField(Lector, related_name = 'participantes_comunidad');
+    nombre = models.CharField(max_length=20);
+    lugares_encuentro = models.ManyToManyField(Espacio, related_name = 'lugares_encuentro');
+    libros = models.ManyToManyField(Info_Libro, related_name = 'libros_comunidad');
+    bibliotecas_amigas = models.ManyToManyField(Biblioteca, related_name = 'biblioteca_amiga');
+    talleres = models.ManyToManyField(Taller, related_name = 'talleres_comunidad');
+    def __unicode__(self):
+        return unicode(self.nombre);
--- lectur/lectur_app/views.py
+++ lectur/lectur_app/views.py
@@ -1,19 +1,115 @@
+# -*- encoding: utf-8 -*-
 from django.shortcuts import render
-
-import datetime;
-from django.views.generic import View, FormView, UpdateView, CreateView, DetailView, ListView, TemplateView;
-from django.http import HttpResponseRedirect, HttpResponse;
+import datetime
 from django.template.defaultfilters import slugify
+
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-# from models import
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
+
+from django.views.generic import View, FormView, UpdateView, CreateView, DetailView, ListView, TemplateView
+from forms import UserForm, LectorForm;
+from models import Lector
+
+from django.conf import settings
+
 
 # Create your views here.
 
+# Create your views here.
 class Home(TemplateView):
-    """docstring for Home."""
-    template_name="val.html"
+    template_name = 'base.html'
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
-        context["usuarios"]=User.objects.all();
-        context["estado"]= 5;
+        context["aaa"]="Nadie";
+        return context
+
+class Register(CreateView):
+	""" Vista de la página de formulario donde se registra un nuevo usuario """
+	template_name = 'register.html'
+	form_class = UserForm
+	model = User
+
+	def form_valid(self, form):
+		user = form.save(commit=False)
+		user.username = user.username.lower()
+		user.set_password(user.password)
+		user.save()
+		p=form.data["password"]
+		u= auth.authenticate(username=user.username, password=p)
+		if u is not None and u.is_active:
+			auth.login(request=self.request, user=u)
+		return HttpResponseRedirect('/register/perfil/')
+
+
+def get_codigo_nuevo_usuario():
+    hoy = datetime.datetime.today();
+    an=hoy.year-2000;
+    mes=hoy.month;
+    if(mes<10):
+        cod=str(an)+"0"+str(mes);
+    else:
+        cod=str(an)+""+str(mes);
+    cant=len(Lector.objects.filter(fecha_ingreso__year=hoy.year, fecha_ingreso__month=hoy.month));
+    if cant < 10:
+        cod=cod+"000";
+    elif cant<100:
+        cod=cod+"00";
+    elif cant<1000:
+        cod=cod+"0";
+
+    cod=cod+str(cant);
+    return cod;
+
+
+class RegisterProfile(CreateView):
+    """ Vista de la página de formulario donde se registra un nuevo usuario """
+    template_name = 'register.html'
+    form_class = LectorForm
+    model = User
+
+    def form_valid(self, form):
+    	lector = form.save(commit=False);
+    	lector.user = self.request.user;
+        if not lector.codigo:
+            lector.codigo=get_codigo_nuevo_usuario();
+    	lector.save();
+        form.save_m2m();
+        return HttpResponseRedirect('/prueba');
+
+class UpdateProfile(UpdateView):
+    """ Vista de la página de formulario donde se registra un nuevo usuario """
+    template_name = 'register.html'
+    form_class = LectorForm
+    model = User
+    state=False
+
+    def get_state(self):
+		return state
+
+    def get_object(self, queryset=None):
+    	user = self.request.user;
+    	obj = Lector.objects.get(user=user)
+    	return obj
+
+    def form_valid(self, form):
+    	lector = form.save();
+        #dir='7perfil/'+lector.codigo
+        return HttpResponseRedirect('/prueba');
+
+
+
+class Prueba(TemplateView):
+    template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super(Prueba, self).get_context_data(**kwargs)
+        user= self.request.user;
+        if user.is_authenticated():
+            userprofile= Lector.objects.get(user=user);
+            info= user.username +" | "+ userprofile.codigo +": "+ userprofile.descripcion;
+            context["aaa"]=info;
+            context["img"]=userprofile.imagen;
         return context
